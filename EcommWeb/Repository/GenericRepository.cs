f7e106ef21876e870384a37a064986a7258a1ee4
NishirajNath01 <nishirajnath01@outlook.com>
2022-11-10 15:40:58 +0530

    Added Repositories

---
+++ EcommWeb/Repository/GenericRepository.cs
@@ -0,0 +1,119 @@
+﻿using EcommWeb.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace EcommWeb.Repository
+{
+    public class GenericRepository<Tbl_Entity> : IRepository<Tbl_Entity> where Tbl_Entity : class
+    {
+        DbSet<Tbl_Entity> _dbSet;
+
+        private dbMyOnlineShoppingEntities _DbEntity;
+
+        public GenericRepository(dbMyOnlineShoppingEntities DbEntity)
+        {
+            _DbEntity = DbEntity;
+            _dbSet = _DbEntity.Set<Tbl_Entity>();
+        }
+
+        public void Add(Tbl_Entity entity)
+        {
+            _dbSet.Add(entity);
+            _DbEntity.SaveChanges();
+        }
+
+        public int GetAllrecordsCount()
+        {
+            return _dbSet.Count();
+        }
+
+        public IEnumerable<Tbl_Entity> GetAllRecords()
+        {
+            return _dbSet.ToList();
+        }
+       
+        public IQueryable<Tbl_Entity> GetAllRecordsIQyeryable()
+        {
+            return _dbSet;
+        }
+
+        public Tbl_Entity GetFirstorDefault(int recordId)
+        {
+            return _dbSet.Find(recordId);
+        }
+
+        public Tbl_Entity GetFirstorDefaultByParameter(Expression<Func<Tbl_Entity, bool>> wherePredict)
+        {
+            return _dbSet.Where(wherePredict).FirstOrDefault();
+        }
+
+        public IEnumerable<Tbl_Entity> GetListParameter(Expression<Func<Tbl_Entity, bool>> wherePredict)
+        {
+            return _dbSet.Where(wherePredict).ToList();
+        }
+
+        public IEnumerable<Tbl_Entity> GetResultBySqlprocedure(string query, params object[] parameters)
+        {
+            if (parameters != null)
+            {
+                return _DbEntity.Database.SqlQuery<Tbl_Entity>(query, parameters).ToList();
+            }
+            else
+                return _DbEntity.Database.SqlQuery<Tbl_Entity>(query).ToList();
+        }
+
+        public void InactiveAndDeleteMarkByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict)
+        {
+            _dbSet.Where(wherePredict).ToList().ForEach(ForEachPredict);
+        }
+
+        public void Remove(Tbl_Entity entity)
+        {
+            if (_DbEntity.Entry(entity).State == EntityState.Detached)
+                _dbSet.Attach(entity);
+            _dbSet.Remove(entity);
+        }
+
+        public void RemovebyWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict)
+        {
+            Tbl_Entity entity = _dbSet.Where(wherePredict).FirstOrDefault();
+            Remove(entity);
+        }
+
+        public void RemoveRangeBywhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict)
+        {
+            List<Tbl_Entity> entity = _dbSet.Where(wherePredict).ToList();
+            foreach(var ent in entity)
+            {
+                Remove(ent);
+            }
+        }
+
+        public void Update(Tbl_Entity entity)
+        {
+            _dbSet.Attach(entity);
+            _DbEntity.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void UpdateByWherClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict)
+        {
+            _dbSet.Where(wherePredict).ToList().ForEach(ForEachPredict);
+        }
+
+        public IEnumerable<Tbl_Entity> GetRecordsToShow(int PageNo, int PageSize, int CurrentPage, Expression<Func<Tbl_Entity, bool>> wherePredict, Expression<Func<Tbl_Entity, int>> orderByPredict)
+        {
+            if(wherePredict != null)
+            {
+                return _dbSet.OrderBy(orderByPredict).Where(wherePredict).ToList();
+            }
+            else
+            {
+                return _dbSet.OrderBy(orderByPredict).ToList();
+            }
+        }
+    }
+}
